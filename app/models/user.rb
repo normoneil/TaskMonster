9628406f79e9000218ab2d3d07bdbc28b4b1372d
normoneil <normoneil@users.noreply.github.com>
2016-12-05 20:41:19 -0600

    Styles and layouts - just starting

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         validates :username, :uniqueness => true
+         validates :username, :presence => true
+         validates :email, :uniqueness => true
+         validates :email, :presence => true
 end
